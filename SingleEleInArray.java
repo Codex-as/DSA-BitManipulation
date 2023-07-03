455fdfccf6aa1f2ce527682ac9dd7ebc84b05e3f
Ashish Ranjan <107981335+Codex-as@users.noreply.github.com>
2023-07-03 22:48:37 +0530

    Create SingleEleInArray.java

---
+++ SingleEleInArray.java
@@ -0,0 +1,12 @@
+public class SingleEleInArray {
+
+	public static void main(String[] args) {
+		int arr[] = {10,20,30,30,40,40,10,50,50};
+		int res = 0;
+		for(int i =0;i<arr.length;i++) {
+			res= res^arr[i];
+		}
+		System.out.println(res);
+	}
+
+}
